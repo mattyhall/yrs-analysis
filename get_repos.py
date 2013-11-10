4fd4acf9d5d8f0853d5e8307fbe8ef10b9eba9e4
Matthew Hall <hailmattyhalluk@gmail.com>
2013-11-10 14:59:52 +0000

    Scrape the hacks.youngrewiredstate.org site.
    Unfortunately my first idea of searching github for YRS projects did not work, as there are many projects there with the text "YRS" that did not take part in the competition. Instead I am going to scrape the site for github project pages, store them in a database and write another script to download the data from that url.

--- get_repos.py
+++ get_repos.py
@@ -1,43 +1,30 @@
 from pymongo import MongoClient
 import requests
-import getpass
-import time
+from bs4 import BeautifulSoup
 
-def get_languages(url, auth):
-    while True:
-        r = requests.get(url, auth=auth)
-        json = r.json()
-        if json.get('documentation_url', '') == 'http://developer.github.com/v3/#rate-limiting':
-            print("Language rate limit reached, sleeping")
-            time.sleep(30)
-            continue
-        return json
-
-user = 'mattyhall'
-password = getpass.getpass()
-url = 'https://api.github.com/search/repositories?q=YRS&page={0}'
-
+base_url = 'http://hacks.youngrewiredstate.org/'
+r = requests.get(base_url+'/events/YRS2013')
+main_page = BeautifulSoup(r.text)
+centre_hrefs = [element.get('href') for element in main_page.select("ul.centre_list > li > a")]
+project_hrefs = []
+for centre in centre_hrefs:
+    r = requests.get(base_url + centre)
+    centre_page = BeautifulSoup(r.text)
+    project_hrefs += [element.get('href') for element in centre_page.select("div.right > h3 > a")]
+github_hrefs = []
+for project in project_hrefs:
+    r = requests.get(base_url + project)
+    project_page = BeautifulSoup(r.text)
+    github = project_page.select("div.inner > ul > li.icon_github")
+    if len(github) != 0:
+        href = github[0].findChild('a').get('href')
+        finalist = False
+        if len(project_page.select("div.award")) != 0:
+            finalist = True
+        github_hrefs.append({'finalist': finalist, 'herf': href})
+print(github_hrefs)
 client = MongoClient()
 db = client.repo_database
-repos = db.repos
-repos.drop()
-
-i = 0
-while True:
-    r = requests.get(url.format(i), auth=(user, password))
-    json = r.json()
-    if json.get('items', None) is None:
-        if json.get('documentation_url', '') == 'http://developer.github.com/v3/#rate-limiting':
-            print('Rate limit exceeded, sleeping')
-            time.sleep(30)
-            continue
-        break
-    print('Got page', str(i))
-    for repo in json['items']:
-        languages = []
-        if repo['language'] != None:
-            languages = get_languages(repo['languages_url'], (user, password))
-        repo['languages'] = languages
-        repos.insert(repo)
-    i += 1
-print(repos.count())
+github_projects = db.github_projects
+github_projects.drop()
+github_projects.insert(github_hrefs)
